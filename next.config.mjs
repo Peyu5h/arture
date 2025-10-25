3e9d5cc3029cf0ace9f41367eaf38f34b919835d
Peyu5h <piyushvbagul916@gmail.com>
2025-10-25 23:01:00 +0530

    fix:prod-fail-test

--- next.config.mjs
+++ next.config.mjs
@@ -23,6 +23,15 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('@prisma/client');
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
